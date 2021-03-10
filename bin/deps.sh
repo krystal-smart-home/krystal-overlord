a6201294fa6980398b6256829380cb131449ef4b
nekoffski <kacperczyz@protonmail.com>
2021-03-10 15:55:12 +0100

    Add missing deps

--- bin/deps.sh
+++ bin/deps.sh
@@ -6,6 +6,8 @@ sudo apt-get install -y g++-10
 sudo apt-get install -y libjsoncpp-dev
 sudo apt-get install -y libspdlog-dev
 
-# boost
+sudo apt-get install -y libgtest-dev
+sudo apt-get install -y libgmock-dev
+
 sudo apt-get install -y libboost-system1.71-dev
 sudo apt-get install -y libboost-coroutine1.71-dev
